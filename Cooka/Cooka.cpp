a79fd5eff19ca568e488beb1f58a95b5432773cd
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2022-12-06 00:59:27 -0500

    merge sort implemented to sort the recipes by highest number of matching ingredients

--- Cooka/Cooka.cpp
+++ Cooka/Cooka.cpp
@@ -70,6 +70,65 @@ void CheckIngredient(string ingredient, vector<Recipe>& recipes) {
     }
 }
 
+// Merge two subarrays from arr
+void merge(vector<Recipe>& recipes, int left, int mid, int right)
+{
+    // Create X -> recipes[left..mid] & Y -> recipes[mid+1..right]
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<Recipe> X(n1), Y(n2);
+    for (int i = 0; i < n1; i++)
+        X[i] = recipes[left + i];
+    for (int j = 0; j < n2; j++)
+        Y[j] = recipes[mid + 1 + j];
+    // Merge the arrays X and Y into recipes vector
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2)
+    {
+        if (X[i].GetCount() >= Y[j].GetCount())
+        {
+            recipes[k] = X[i];
+            i++;
+        }
+        else
+        {
+            recipes[k] = Y[j];
+            j++;
+        }
+        k++;
+    }
+    // When we run out of elements in either X or Y append the remaining elements
+    while (i < n1)
+    {
+        recipes[k] = X[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        recipes[k] = Y[j];
+        j++;
+        k++;
+    }
+}
+
+// mergeSort and merge code from lecture module 6 - sorting
+void mergeSort(vector<Recipe>& recipes, int left, int right)
+{
+    if (left < right)
+    {
+        // m is the point where the array is divided into two subarrays
+        int mid = left + (right - left) / 2;
+        mergeSort(recipes, left, mid);
+        mergeSort(recipes, mid + 1, right);
+        // Merge the sorted subarrays
+        merge(recipes, left, mid, right);
+    }
+}
+
 int main()
 {
     vector<Recipe> recipes;
@@ -96,7 +155,17 @@ int main()
                 cin >> ingredient;
                 CheckIngredient(ingredient, recipes);
             }
-            cout << recipes[0].GetName() << ": " << recipes[0].GetCount() << endl;
+        }
+        cout << recipes.size() << " results" << endl;
+        cout << "Select sorting algorithm: " << endl;
+        cout << "1. Shell Sort" << endl;
+        cout << "2. Merge Sort" << endl;
+        cin >> userInput;
+        if (userInput == 2) {
+            mergeSort(recipes, 0, recipes.size() - 1);
+            for (int i = 0; i < recipes.size(); i++) {
+                cout << recipes.at(i).GetName() << ": " << recipes.at(i).GetCount() << endl;
+            }
         }
     }
 }
